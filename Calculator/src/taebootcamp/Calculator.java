50f0b5221463b1853134a39aa38f8d8a87bbd834
pilimeli <82001658+pilimeli@users.noreply.github.com>
2021-04-06 12:07:58 -0500

    Proyecto Java

---
+++ Calculator/src/taebootcamp/Calculator.java
@@ -0,0 +1,11 @@
+package taebootcamp;
+
+public class Calculator {
+	public static void main (String[] args) {
+		int sum=0;
+		int a = 1;
+		int b = 2;
+		sum = (a+b);
+		System.out.println("resultado de la suma:"+sum);
+		}
+}
