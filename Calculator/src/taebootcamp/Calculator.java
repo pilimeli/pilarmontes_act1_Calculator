b17e694090bb544defc488b8362b25f74d36ec39
pilimeli <82001658+pilimeli@users.noreply.github.com>
2021-04-06 19:42:40 -0500

    Metodo suma y resta

--- Calculator/src/taebootcamp/Calculator.java
+++ Calculator/src/taebootcamp/Calculator.java
@@ -1,11 +1,38 @@
 package taebootcamp;
 
+import java.util.Scanner;
+
 public class Calculator {
-	public static void main (String[] args) {
-		int sum=0;
-		int a = 1;
-		int b = 2;
-		sum = (a+b);
-		System.out.println("resultado de la suma:"+sum);
-		}
+
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+
+	public static int Substract(int c, int d) {
+		return c - d;
+	}
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("SUMA");
+		System.out.print("Introduzca primer número: ");
+		
+		int a = teclado.nextInt();
+		System.out.print("Introduzca segundo número: ");
+		
+		int b = teclado.nextInt();
+		System.out.println(a + "+" + b + "=" + (a + b));
+		
+		System.out.println("RESTA");
+		System.out.print("Introduzca primer número: ");
+		
+		int c = teclado.nextInt();
+		System.out.print("Introduzca segundo número: ");
+		int d = teclado.nextInt();
+		
+		System.out.println(c + "-" + d + "=" + (c - d));
+
+	}
+
 }
